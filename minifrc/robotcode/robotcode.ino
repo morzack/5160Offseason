2984d508b587ffdfb79948edb2bcf236639094aa
John Kesler <John_Kesler@caryacademy.org>
2019-05-28 08:29:45 -0400

    Added code (run at competition)

--- minifrc/robotcode/robotcode.ino
+++ minifrc/robotcode/robotcode.ino
@@ -1,130 +1,128 @@
-// libraries
-#include <SoftwareSerial.h>
-#include <AFMotor.h>
-#include <SimpleSoftwareServo.h>
+//instructions for downloading and installing libraries can be found here: https://www.instructables.com/id/Downloading-All-the-Software-Youll-Need-for-MiniFR/
+#include <SoftwareSerial.h>         //this library is part of Arduino by default
+#include <AFMotor.h>                //you must download and install this library: https://drive.google.com/file/d/1zsMywqJjvzgMBoVZyrYly-2hXePFXFzw/view?usp=sharing
+#include <SimpleSoftwareServo.h>    //you must download and install this library: https://drive.google.com/open?id=12Yz_uNNuAiASnTsu424Bm_q8Sj3SzlFx
+                                  
+/* <==============================================================>
+ *  You will need to change the following variables depending on what
+ *  analog pins on your motor shield you are using, which motor goes to
+ *  which port, and if your drive logic is flipped. */
 
-// objects
-SoftwareSerial bluetooth(A0, A1);
+//change A0 and A1 to match whatever pins you are useing for your bluetooth chip
+SoftwareSerial bluetooth(A0, A1); //RX,TX
 
-// TODO Proper configuration!!
+//These lines declare which ports your motors will be connected to on the motor shield.
+AF_DCMotor mLeft(3);
+AF_DCMotor mRight(2);
+AF_DCMotor mLift(1);
 
-// motors
-AF_DCMotor leftDriveMotor(1);
-AF_DCMotor rightDriveMotor(2);
-AF_DCMotor liftMotor(3);
+//this line creates a servo called "servo1"
+SimpleSoftwareServo servo1;
+SimpleSoftwareServo servo2;
 
-// servos
-SimpleSoftwareServo rotator;
-SimpleSoftwareServo grabber;
-float rotatorPosition = 200;
+float xAxisMultiplier = 0.95;      // Change this variable to -1 if your robot turns the wrong way
+float yAxisMultiplier = 0.95;       // Change ths variable to -1 if your robot drives backward when it should be going forward
 
-// input handling things
-float xInputMultiplier = 1;
-float yInputMultiplier = 1;
+/* You shouldn't need to change anything past here unless you're adding
+ *  something like an automode, extra motor, or servo. 
+ *  <==============================================================> */
 
-// input variables
-float xInput, yInput, liftInput, rotatorInput, grabberInput, autonomousInput;
-// note the first 3 required a lowered lift
-float groundPosInput, lv1PosInput, lv2PosInput, lv3PosInput;
+// Variables used to recive data from the driverstation and calculate drive logic
+float xAxis, yAxis, liftAxis;
+int velocityL, velocityR;
 
-bool driverControlled = true;
+int rotatorPosition = 120;
 
-// programmed rotator positions; index goes with position from gnd to lv3
-int rotatorPositions[4] = {10, 30, 70, 55};
+bool autonomous = false;
 
-// arduino setup
+//this variable is used to control your servo
+float button, rotator, bottom, middle, top, ubertop;
+
+// In setup, we tell bluetooth communication to start and set all of our motors to not move
 void setup() {
-    bluetooth.begin(9600);
+  bluetooth.begin(9600);
+  drive(0, 0);
+  servo1.attach(9);          //this line tells the robot that your servo is on pin 9. (pin 9 is servo port 1, pin 10 is servo port 2)
+  servo2.attach(10);
 }
 
 void loop() {
-    // handle user input
-    // it'll be formatted as x1,x2,y,beastMode
-    while (bluetooth.available() > 0) {
-        if (bluetooth.read() == 'z') { // iirc z is the delimiter for the data sent
-            // grab the raw user input
-            // drive controls
-            xInput = bluetooth.parseFloat() * 100 * xInputMultiplier;
-            yInput = bluetooth.parseFloat() * 100 * yInputMultiplier;
-            // lift controls
-            liftInput = bluetooth.parseFloat() * 100;
-            // grabber control
-            grabberInput = bluetooth.parseFloat();
-            // rotator arm control and positions
-            rotatorInput = bluetooth.parseFloat();
-            groundPosInput = bluetooth.parseFloat();
-            lv1PosInput = bluetooth.parseFloat();
-            lv2PosInput = bluetooth.parseFloat();
-            lv3PosInput = bluetooth.parseFloat();
-            autonomousInput = bluetooth.parseFloat();
+  while(bluetooth.available() > 0){                                   // This line checks for any new data in the buffer from the driverstation
+    if ((bluetooth.read()) == 'z'){                                   // We use 'z' as a delimiter to ensure that the data doesn't desync
+      yAxis = (bluetooth.parseFloat()) * (100) * yAxisMultiplier;
+      xAxis = (bluetooth.parseFloat()) * (100) * xAxisMultiplier;     // For each item the driver station sends, we have a variable here to recieve it
+      liftAxis = (bluetooth.parseFloat() * (100));
 
-            // set rotator position
-            if (groundPosInput == 1) {
-                rotatorPosition = rotatorPositions[0];
-            } else if (lv1PosInput == 1) {
-                rotatorPosition = rotatorPositions[1];
-            } else if (lv2PosInput == 1) {
-                rotatorPosition = rotatorPositions[2];
-            } else if (lv3PosInput == 1) {
-                rotatorPosition = rotatorPositions[3];
-            }
-            
-            // update rotator based on input
-            if (rotatorInput == 1) {
-                rotatorPosition -= 10;
-            } else if (rotatorInput == -1) { 
-                rotatorPosition -= 10;
-            }
-        }
+      button = bluetooth.parseFloat();
+      rotator = bluetooth.parseFloat();
+      bottom = bluetooth.parseFloat();
+      middle = bluetooth.parseFloat();
+      top = bluetooth.parseFloat();
+      ubertop = bluetooth.parseFloat();
+      autonomous = bluetooth.parseFloat()==1;
 
-        if (driverControlled) {
-            // update things
-            moveRotator(rotatorPosition);
-            updateGrabber(grabberInput == 1);
-            drive(xInput, yInput);
-            lift(liftInput);
-        }
-        else {
-            // yeet
-            drive(0, 100);
-            delay(3*1000);
-            drive(0, 0);
-            driverControlled = true;
-        }
-    }
+      if (bottom==1) {
+        rotatorPosition = 10;
+      }
+      if (middle==1) {
+        rotatorPosition = 27;
+      }
+      if (top==1) {
+        rotatorPosition = 70;
+      }
+      if (ubertop==1) {
+        rotatorPosition = 55;
+      }
+
+      //these lines control your servo. You may have to change them in order to get the desired result from your servo
+      if (button == 1){
+        servo1.write(179-60);
+      } else {
+        servo1.write(179);
+      }
+
+      //these lines control your servo. You may have to change them in order to get the desired result from your servo
+      if (rotator == 1){
+        rotatorPosition-=10;
+      } else if (rotator == -1) {
+        rotatorPosition+=10;
+      }
+
+      rotatorPosition = constrain(rotatorPosition, 5, 120);
+      servo2.write(rotatorPosition);
+      SimpleSoftwareServo::refresh();
+
+    } 
+  }
+  
+  if (autonomous) {
+    drive(0, 100);
+    lift(0);
+    delay(1500);
+    drive(0, 0);
+    autonomous = false;
+  }
+  else {
+    // This line tells the drive function what speed and direction to move the motors in
+    drive(-xAxis, yAxis);
+    lift(liftAxis);
+  }
 }
 
-// honestly none of this really matters if we use kbd to drive
-float handleDeadzone(float i, int deadzone) {
-    // handle a deadzone
-    return (abs(i)>deadzone)?i:0;
+// This function handles drive logic and actuation. Don't change this unless you know what you're doing.
+void drive(int xAxis, int yAxis) {
+  float V = (100 - abs(xAxis)) * (yAxis/100) + yAxis;    // This is where the X and Y axis inputs are converted into tank drive logic
+  float W = (100 - abs(yAxis)) * (xAxis/100) + xAxis;
+  velocityL = ((((V-W)/2)/100)*255);
+  velocityR = ((((V+W)/2)/100)*255);
+
+  mRight.run((velocityR >= 0) ? FORWARD : BACKWARD);     // These comands tell the motors what speed and direction to move at
+  mRight.setSpeed(abs(velocityR));
+  mLeft.run((velocityL >= 0) ? FORWARD : BACKWARD);
+  mLeft.setSpeed(abs(velocityL));
 }
 
-float curveInput(float i) {
-    // we'll use x^3 for motor curving because why not
-    return i*i*i;
-}
-
-void drive(float x, float y) {
-    float v = (100 - abs(x)) * (y/100) + y;
-    float w = (100 - abs(y)) * (x/100) + x;
-    float lPower = (v-w)*1.275;
-    float rPower = (v+w)*1.275;
-    rightDriveMotor.run(rPower >= 0 ? FORWARD : BACKWARD);
-    leftDriveMotor.run(lPower >= 0 ? FORWARD : BACKWARD);
-    rightDriveMotor.setSpeed(abs(rPower));
-    leftDriveMotor.setSpeed(abs(lPower));
-}
-
-void lift(float l) {
-    liftMotor.run(l >= 0 ? FORWARD : BACKWARD);
-    liftMotor.setSpeed(abs(l)/2);
-}
-
-void moveRotator(int pos) {
-    rotator.write(constrain(pos, 10, 120));
-}
-
-void updateGrabber(bool open) {
-    grabber.write(open ? 119 : 179);
+void lift(float liftPower) {
+  mLift.run((liftPower>=0)?FORWARD:BACKWARD);
+  mLift.setSpeed(liftPower);
 }
